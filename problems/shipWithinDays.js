5b0407e32318d358330ac351d497f295d8518186
Howard Yang <howardyang25@gmail.com>
2019-10-16 19:26:14 -0700

    Implemented shipWithinDays with binary search

--- problems/shipWithinDays.js
+++ problems/shipWithinDays.js
@@ -21,5 +21,36 @@ const shipWithinDays = (weights, D) => {
   }
 }
 
+const shipWithinDaysOptimized = (weights, D) => {
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < weights.length; i++) {
+    left = Math.max(left, weights[i]);
+    right += weights[i];
+  }
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    let need = 1;
+    let cur = 0;
+    for (let i = 0; i < weights.length; i++) {
+      if (cur + weights[i] > mid) {
+        need += 1;
+        cur = 0;
+      }
+
+      cur += weights[i];
+    }
+
+    if (need > D) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
+
 const weights = [1,2,3,4,5,6,7,8,9,10];
-console.log(shipWithinDays(weights, 5));
+console.log(shipWithinDaysOptimized(weights, 5));
