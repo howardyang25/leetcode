15f12f7353e6d7c321c8ce974bb02793c14a1966
Howard Yang <howardyang25@gmail.com>
2019-10-22 11:49:17 -0700

    Implement merge sort

---
+++ problems/mergeSort.js
@@ -0,0 +1,31 @@
+const merge = (left, right) => {
+  const resultArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return resultArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  
+  return merge(mergeSort(left), mergeSort(right));
+};
+const arr = [5, 2, 7, 6, 4, 1, 3, 9, 8];
+console.log(mergeSort(arr));
